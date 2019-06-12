8abfd7178ae36d4512c15faf612d0ef56cfa46e1
Agnes NM <agnesndutamwaniki@gmail.com>
2019-06-12 08:48:14 +0300

    create basic operator functions

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//Basic math operator functions
+function add (a,b){
+    let sum = a+b;
+    return sum;
+}
+
+add(2,3);
+
+function subtract (c,d){
+    let subt = c-d;
+    return subt;
+}
+
+subtract(3,2);
+
+function multiply(e,f){
+    let mult = e*f;
+    return mult;
+}
+
+multiply(2,3);
+
+function division (g,h){
+    let divide = g/h;
+    return divide;    
+}
+
+division(6,2);
